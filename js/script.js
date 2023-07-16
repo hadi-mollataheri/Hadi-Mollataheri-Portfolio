d45d885760f89e485adae2adb47795f4c70978f0
Hadi-Mollataheri <mhadimollataheri@gmail.com>
2023-07-16 16:05:51 +0330

    Select 0 index of themeButton

--- js/script.js
+++ js/script.js
@@ -1,5 +1,2 @@
 // Select the button with theme class
-const themeButton = window.document.getElementsByClassName('theme');
-// Add an Event Handler for it(name and function)
-// Select the main element
-// When this element is clicked turn the background color of the main element to white and the color to black
+const themeButton = document.getElementsByClassName('theme')[0];
