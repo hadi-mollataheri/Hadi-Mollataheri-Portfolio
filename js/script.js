647bee757edc8839996007413ce34906b553e04d
Hadi-Mollataheri <mhadimollataheri@gmail.com>
2023-07-16 16:08:35 +0330

    Select the body element

--- js/script.js
+++ js/script.js
@@ -1,2 +1,4 @@
 // Select the button with theme class
 const themeButton = document.getElementsByClassName('theme')[0];
+// Select the body element
+const body = document.getElementsByTagName('body')[0];
