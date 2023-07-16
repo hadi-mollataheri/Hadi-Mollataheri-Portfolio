63ec4d73f1cb36f451ec83bacef25b0fe7dc7d52
Hadi-Mollataheri <mhadimollataheri@gmail.com>
2023-07-16 16:10:13 +0330

    Write the Event handler function for changing color & back color of body elem

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@
 const themeButton = document.getElementsByClassName('theme')[0];
 // Select the body element
 const body = document.getElementsByTagName('body')[0];
+// When this element is clicked turn the background color of the main element to white and the color to black
+// Write the Event handler function
+function changeColors() {
+    body.style.backgroundColor = '#FFF';
+    body.style.color = '#000';
+}
+// Add an Event Handler for it(name and function)
+themeButton.addEventListener('click', changeColors);
