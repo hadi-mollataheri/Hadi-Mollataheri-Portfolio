a81928717d5be2940913c29d1f3260ef76a5e2c3
Hadi-Mollataheri <mhadimollataheri@gmail.com>
2023-07-26 14:24:57 +0330

    Style the prjects and add clickability to thefirst of it

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 const themeToggleButton = document.querySelector("button.theme-toggle-button");
 
 function changeTheme() {
-    // Selecting all the elements the their styles need to be changed
+  // Selecting all the elements the their styles need to be changed
   document.body.classList.toggle("light");
   document.querySelector("nav").classList.toggle("light");
   document.querySelector(".nav-right a").classList.toggle("light");
-  document.querySelector("button.theme-toggle-button").classList.toggle("light");
+  document
+    .querySelector("button.theme-toggle-button")
+    .classList.toggle("light");
   document.querySelector("svg").classList.toggle("light");
   document.querySelector("section.for-light-theme1").classList.toggle("light");
   document.querySelector("section.for-light-theme2").classList.toggle("light");
@@ -14,4 +16,16 @@ function changeTheme() {
 
 themeToggleButton.addEventListener("click", changeTheme);
 
+/* Create clickability  for tribute projects*/
+const tribute = document.querySelector("li.tribute");
+function showDescription() {
+  document
+    .querySelector("div.description")
+    .classList.toggle("description-tribute");
+}
+tribute.addEventListener("click", showDescription);
 
+/* Create clickability  for responsive projects*/
+
+
+/* Fix the hover button for the light theme for subject of projects */
